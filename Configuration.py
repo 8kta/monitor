8ab28f30d07a591a3bcde420b2482c1111b75a0a
8kta <alonsog.gascon@gmail.com>
2023-09-16 21:02:35 -0400

    Ready for test

--- Configuration.py
+++ Configuration.py
@@ -29,14 +29,7 @@ class Configuration:
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super(Configuration, cls).__new__(cls)
-            # cls._instance.value = None
         return cls._instance
-
-    # def set_value(self, value):
-    #     self.value = value
-    #
-    # def get_value(self):
-    #     return self.value
 
     def get_source(self):
         self.singleton()
--- LifeIngest.py
+++ LifeIngest.py
@@ -7,10 +7,11 @@ from Configuration import Configuration
 
 
 class LifeIngest(FileSystemEventHandler):
+    configuration = Configuration()
+    source = configuration.get_source()
+    target = configuration.get_target()
 
-    def __init__(self, source_dir, target_directory):
-        self.source_directory = source_dir
-        self.target_directory = target_directory
+    def __init__(self):
         self.observer = Observer()
 
     def on_created(self, event):
@@ -21,39 +22,27 @@ class LifeIngest(FileSystemEventHandler):
         print(f"File created: {event.src_path}")
 
     def ingest(self, source_file):
-        target_directory = self.target_directory
-        shutil.copy2(source_file, target_directory)
+        target_directory = self.target
+        try:
+            shutil.copy2(source_file, target_directory)
+        except OSError as err:
+            print(err)
 
+    def start_observing(self):
+        observer = Observer()
+        observer.schedule(self, path=self.source, recursive=False)
+        observer.start()
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
 
-def main(event_handler):
-    observer = Observer()
-    observer.schedule(event_handler, path=event_handler.source_directory, recursive=False)
-    observer.start()
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+    def start(self):
+        self.start_observing()
 
 
 if __name__ == "__main__":
-    configuration = Configuration()
-    source = configuration.get_source()
-    target = configuration.get_target()
-
-    print(source)
-    print(target)
-
-
-    # It doesn't work with cloud (Google Drive) targets. What about NFS?
-    # cloud_target = "C:\\Users\\octav\\OneDrive\\Desktop\\target"
-    # gcloud_target = "G:\\My Drive\\test"
-    # local_target = "C:\\Users\\octav\\Documents\\targetNotCloud"
-    #
-    # # Define the directory to monitor
-    # source_directory = "C:\\Users\\octav\\Documents\\TestMonitor"
-    # cloud_source = "C:\\Users\\octav\\OneDrive\\Desktop\\source"
-
-    ingestion_handler = LifeIngest(source, target)
-    main(ingestion_handler)
+    life_ingestion = LifeIngest()
+    LifeIngest.start(life_ingestion)
